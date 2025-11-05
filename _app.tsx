1fcd36d02cfd717ad44b569f9035e5d954650400
Kechpir <74138894+Kechpir@users.noreply.github.com>
2025-11-06 00:39:21 +0500

    meow

--- _app.tsx
+++ _app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import "../styles/main.css"; // ✅ подключаем новый кастомный стиль
+//import "../styles/main.css"; // ✅ подключаем новый кастомный стиль
 import "../styles/sql-interface.css";
 
 export default function App({ Component, pageProps }: AppProps) {
